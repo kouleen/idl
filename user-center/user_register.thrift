385c1fd4b9a6db0d760659c6c94f3dc85ae581b7
zhangqing.mmqg <zhangqing.mmqg@bytedance.com>
2026-08-24 16:39:19 +0800

    增加注册v1

--- user-center/user_register.thrift
+++ user-center/user_register.thrift
@@ -4,7 +4,7 @@ namespace py user
 
 struct RegisterRequest {
     1: string username      // 用户名称
-    2: string password
+    2: string password      // 用户密码
     3: string nickname
     4: string gender
     5: string phone
