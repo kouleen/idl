cd41b8702513f1066e0b3833ee06d2e30773814c
zhangqing.mmqg <zhangqing.mmqg@bytedance.com>
2026-09-22 16:06:12 +0800

    更新api v12

--- system-center/system_education.thrift
+++ system-center/system_education.thrift
@@ -16,6 +16,7 @@ struct SystemEducationRequest {
     9: optional i64 updatedBy
     10: optional i64 createTime
     11: optional i64 updateTime
+    12: optional list<i64> idList
 
     255: optional base_query.BaseQueryRequest params
 }
